fa460fff5488dac89a15c09679cd70b3610f2eb4
Matthew Engler <matthew.engler.370@my.csun.edu>
2022-04-08 23:18:55 -0700

    Initial Commit

---
+++ BST/src/BinarySearchTree.java
@@ -0,0 +1,8 @@
+
+public class BinarySearchTree {
+
+	public static void main(String[] args) 
+	{
+		
+	}
+}
---
+++ BST/src/BinarySearchTreeNode.java
@@ -0,0 +1,9 @@
+
+public class BinarySearchTreeNode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
